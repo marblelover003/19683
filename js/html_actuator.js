2063cb38160fce39b97d3be00a4cd430b541ab63
Scott Carroll <scott_carroll19@yahoo.com>
2019-03-18 14:52:04 -0500

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -57,7 +57,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   // We can't use classlist because it somehow glitches when replacing classes
   var classes = ["tile", "tile-" + tile.value, positionClass];
 
-  if (tile.value > 6561) classes.push("tile-super");
+  if (tile.value > 19683) classes.push("tile-super");
 
   this.applyClasses(wrapper, classes);
 
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "You won! Keep playing?" : "Game over!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
